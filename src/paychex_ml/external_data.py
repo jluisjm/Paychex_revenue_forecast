3fe7e4f716902450d1e5fc3edf8e89685cf08de3
Bruno <brunocgf@gmail.com>
2022-04-07 14:14:06 -0600

    external data cont'

--- src/paychex_ml/external_data.py
+++ src/paychex_ml/external_data.py
@@ -6,7 +6,8 @@ from azure.storage.blob import BlobServiceClient
 from src.paychex_ml.utils import load_credentials
 from src.paychex_ml.utils import upload_df_parquet
 
-def get_unemployment_rate(seriesid='LNS14000000', startyear='2015', endyear='2022'):
+
+def get_bls_data(seriesid, startyear='2015', endyear='2022'):
     """
     :param seriesid:
     :param startyear:
@@ -14,9 +15,14 @@ def get_unemployment_rate(seriesid='LNS14000000', startyear='2015', endyear='202
     :return:
     """
 
+    # Names dictionary
+    series_dic = {"LNS14000000": 'UnemploymentRate',
+                  "CES0000000001": "NFPayrolls_sa",
+                  "CEU0000000001": "NFPayrolls_nsa"}
+
     # Request data
     headers = {'Content-type': 'application/json'}
-    data = json.dumps({"seriesid": [seriesid],"startyear":startyear, "endyear":endyear})
+    data = json.dumps({"seriesid": seriesid, "startyear": startyear, "endyear": endyear})
     p = requests.post('https://api.bls.gov/publicAPI/v2/timeseries/data/', data=data, headers=headers)
 
     # Load data
@@ -24,15 +30,31 @@ def get_unemployment_rate(seriesid='LNS14000000', startyear='2015', endyear='202
     json_data = json_response['Results']['series'][0]['data']
 
     # Data as dataframe
-    df = pd.DataFrame(json_data).astype({'value': 'float'})
-    df = df.drop(columns='footnotes')
-    df['date'] = pd.to_datetime(df['year'] + df['periodName']+'01', format="%Y%B%d").dt.strftime("%Y%m%d")
+    list_df = []
+    for s in json_response['Results']['series']:
 
-    print("Loaded {} data".format(seriesid))
-    return df[['date','latest','value']]
+        print("Processing: ", s['seriesID'])
+
+        try:
+            col_name = series_dic[s['seriesID']]
+        except:
+            col_name = s['seriesID']
+
+        ser = pd.DataFrame(s['data']) \
+            .astype({'value': 'float'}) \
+            .drop(columns=['latest', 'footnotes', 'period']) \
+            .rename(columns={'value': col_name}) \
+            .set_index(['year', 'periodName'])
+        list_df.append(ser)
+
+    df = pd.concat(list_df, axis=1).reset_index()
+    df['date'] = pd.to_datetime(df['year'] + df['periodName'] + '01', format="%Y%B%d").dt.strftime("%Y%m%d")
+    df = df.drop(columns=['year', 'periodName'])
+
+    return df[['date'] + list(filter(lambda x: x != 'date', df.columns.values))]
+
 
 if __name__ == '__main__':
-
     # Load credentials
     credentials = load_credentials("blob_storage")
 
@@ -40,9 +62,9 @@ if __name__ == '__main__':
     blob_service_client = BlobServiceClient.from_connection_string(credentials['conn_string'])
 
     # Get unemployment rate
-    print
-    df = get_unemployment_rate()
+    seriesid = ["LNS14000000", "CEU0000000001", "CES0000000001"]
+    df = get_bls_data(seriesid)
 
     # Upload data
 
-    blob_client = upload_df_parquet(df, "unemployment_rate.parquet", blob_service_client, container='external-data')
+    blob_client = upload_df_parquet(df, "external_data.parquet", blob_service_client, container='external-data')
