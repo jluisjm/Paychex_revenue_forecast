6b064310d1927615f3b3531293de806d7bfaa9df
Bruno <brunocgf@gmail.com>
2022-05-20 11:00:49 -0500

    error eliminating calendar date

--- run_analysis.py
+++ run_analysis.py
@@ -148,6 +148,7 @@ if __name__=="__main__":
         # ------------------------------------------------------------------------------------- #
         #keeps = ['Calendar Date', target_col]+feature_cols
         keeps = [target_col]+feature_cols
+        train_df = train_df[keeps]
         test_df = test_df[keeps]
         comb_df = comb_df[keeps]
 
